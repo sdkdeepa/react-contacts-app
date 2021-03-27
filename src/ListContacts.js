75e166365ecd2b3ab0de070f66f1402e54115183
sdkdeepa <deepa94086@gmail.com>
2021-03-27 15:23:57 -0700

    accessing values from the component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,9 +4,16 @@ import React, {Component} from 'react'
 class ListContacts extends Component { //do defalt export after this line
 
 render() { //render() describes UI of the component
-    console.log('Props', this.props)
+    // console.log('Props', this.props)
     return (
         <ol className ='contact-list'>
+            {this.props.contacts.map((contact)=> (
+            <li key={contact.id}>
+
+                {contact.name}
+            </li>
+            
+        ))}
 
         </ol>
 
